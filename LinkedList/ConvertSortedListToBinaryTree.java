3dac55802f9a4dd085bd064ca6acbcccb0c2dd30
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-06-29 23:24:30 +0530

    Create ConvertSortedListToBinaryTree.java

---
+++ LinkedList/ConvertSortedListToBinaryTree.java
@@ -0,0 +1,173 @@
+/*
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted linked list: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+ 
+ */
+ 
+ /* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null)
+            return null;
+        int count = getNodeCount(head);
+        return createTree(head, count);
+            
+    }
+    TreeNode createTree(ListNode head, int count)
+    {
+        if(head == null || count <= 0)
+            return null;
+        TreeNode left = createTree(head,count/2);
+        ListNode position = findNode(head,count/2);
+        System.out.println(position.val);
+        TreeNode root = new TreeNode(position.val);
+        root.left = left;
+        root.right = createTree(position.next, count-count/2-1);
+        return root;
+    }
+    ListNode findNode(ListNode head, int k)
+    {
+        // if(head == null || k == 0)
+        //     return null;
+        if(k == 0)
+            return head;
+        ListNode current = head;
+        int i =0;
+        while(current != null && i <k)
+        {
+            current = current.next;
+            i++;
+        }
+        return current;
+    }
+    int getNodeCount(ListNode head)
+    {
+        if(head == null)
+            return 0;
+        ListNode current = head;
+        int count = 0;
+        while(current != null)
+        {
+            current = current.next;
+            count++;
+        }
+        return count;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+    
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (node == null) {
+              output += "null, ";
+              continue;
+            }
+    
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            
+            TreeNode ret = new Solution().sortedListToBST(head);
+            
+            String out = treeNodeToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
