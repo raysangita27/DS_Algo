f53fcbf06132ceb4a3c36c1de37739ce65accb84
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-06-29 22:52:27 +0530

    Create RemoveDuplicate.java

---
+++ LinkedList/RemoveDuplicate.java
@@ -0,0 +1,113 @@
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
+*/
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+    
+          if(head == null)
+            return null;
+        ListNode current = head;
+    
+        while(current != null)
+        {
+            ListNode temp = current;
+            while(temp!= null && temp.val == current.val)
+            {
+                temp = temp.next;
+            }
+            current.next = temp;
+            current = current.next;
+        }
+        return head;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+    
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            
+            ListNode ret = new Solution().deleteDuplicates(head);
+            
+            String out = listNodeToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
