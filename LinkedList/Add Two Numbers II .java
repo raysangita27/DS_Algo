ee3f047817d1c8b8950fab44a250e2db8eb8f320
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-01 02:41:18 +0530

    Create Add Two Numbers II .java

---
+++ LinkedList/Add Two Numbers II .java
@@ -0,0 +1,146 @@
+/*
+
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Follow up:
+What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+
+Example:
+
+Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 8 -> 0 -> 7
+
+*/
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    ListNode result = null;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        Stack<Integer> stk1 = new Stack<>();
+        Stack<Integer> stk2 = new Stack<>();
+        while(l1 != null)
+        {
+            stk1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null)
+        {
+            stk2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        int carry = 0;
+        while(!stk1.isEmpty() || !stk2.isEmpty())
+        {
+            int a = stk1.size()>0 ? stk1.pop(): 0;
+            int b = stk2.size()>0 ? stk2.pop(): 0;
+            int sum = a +b + carry;
+            insertNodeAtHead( sum % 10);
+            // current = current.next;
+            carry = sum / 10;    
+        }
+        if(carry > 0)
+            insertNodeAtHead(carry);
+        return result;
+        
+        
+    }
+    void insertNodeAtHead(int val)
+    {
+        ListNode temp = new ListNode(val);
+        if(result == null)
+        {
+            result = temp;
+            return;
+        }
+        temp.next = result;
+        result = temp;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+    
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode l1 = stringToListNode(line);
+            line = in.readLine();
+            ListNode l2 = stringToListNode(line);
+            
+            ListNode ret = new Solution().addTwoNumbers(l1, l2);
+            
+            String out = listNodeToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
